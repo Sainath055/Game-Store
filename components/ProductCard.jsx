ecb0f3fd53ef1c842ea850af634ffb695963d22a
Sainath055 <nathsai055@gmail.com>
2023-07-08 10:24:43 +0530

    Fixed

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -101,33 +101,35 @@ const ProductCard = ({data, userId}) => {
 
       <div className='w-full h-max relative flex items-center justify-center'>
 
-        <button onClick={handleFav} disabled={likeLoading}
-        className='plusBtn w-max h-max absolute top-2 right-2.5 bg-[#525252bf] 
-          items-center justify-center rounded-full p-1.5 hidden backdrop-blur-sm'>
-          {likeLoading ? 
-            <Ring 
-            size={21}
-            lineWeight={4}
-            speed={2} 
-            color="white" 
-            />
-            :
-            (!heartIcon ? 
-            <svg className='w-[22px] h-[22px]' 
-                xmlns="http://www.w3.org/2000/svg" 
-                viewBox="0 0 24 24">
-                <path fill="currentColor" 
-                d="m10.65 19.8l-1.725-1.575q-2.65-2.425-4.788-4.813T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.325 0 2.5.562t2 1.538q.825-.975 2-1.538t2.5-.562q2.35 0 3.925 1.575T22 8.15q0 2.875-2.125 5.275T15.05 18.25l-1.7 1.55q-.575.55-1.35.55t-1.35-.55Zm.4-13.05q-.725-1.025-1.55-1.563t-2-.537q-1.5 0-2.5 1t-1 2.5q0 1.3.925 2.763t2.213 2.837q1.287 1.375 2.65 2.575T12 18.3q.85-.775 2.213-1.975t2.65-2.575q1.287-1.375 2.212-2.837T20 8.15q0-1.5-1-2.5t-2.5-1q-1.175 0-2 .537T12.95 6.75q-.175.25-.425.375T12 7.25q-.275 0-.525-.125t-.425-.375Zm.95 4.725Z"/>
-            </svg>
-            :
-            <svg className='w-[22px] h-[22px]' 
-            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path fill="currentColor" 
-                d="M13.35 20.13c-.76.69-1.93.69-2.69-.01l-.11-.1C5.3 15.27 1.87 12.16 2 8.28c.06-1.7.93-3.33 2.34-4.29c2.64-1.8 5.9-.96 7.66 1.1c1.76-2.06 5.02-2.91 7.66-1.1c1.41.96 2.28 2.59 2.34 4.29c.14 3.88-3.3 6.99-8.55 11.76l-.1.09z"></path>
-            </svg>
-            )
-          }
-        </button>
+        {data?.library.includes(String(userId)) ? null : 
+          <button onClick={handleFav} disabled={likeLoading}
+          className='plusBtn w-max h-max absolute top-2 right-2.5 bg-[#525252bf] 
+            items-center justify-center rounded-full p-1.5 hidden backdrop-blur-sm'>
+            {likeLoading ? 
+              <Ring 
+              size={21}
+              lineWeight={4}
+              speed={2} 
+              color="white" 
+              />
+              :
+              (!heartIcon ? 
+              <svg className='w-[22px] h-[22px]' 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  viewBox="0 0 24 24">
+                  <path fill="currentColor" 
+                  d="m10.65 19.8l-1.725-1.575q-2.65-2.425-4.788-4.813T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.325 0 2.5.562t2 1.538q.825-.975 2-1.538t2.5-.562q2.35 0 3.925 1.575T22 8.15q0 2.875-2.125 5.275T15.05 18.25l-1.7 1.55q-.575.55-1.35.55t-1.35-.55Zm.4-13.05q-.725-1.025-1.55-1.563t-2-.537q-1.5 0-2.5 1t-1 2.5q0 1.3.925 2.763t2.213 2.837q1.287 1.375 2.65 2.575T12 18.3q.85-.775 2.213-1.975t2.65-2.575q1.287-1.375 2.212-2.837T20 8.15q0-1.5-1-2.5t-2.5-1q-1.175 0-2 .537T12.95 6.75q-.175.25-.425.375T12 7.25q-.275 0-.525-.125t-.425-.375Zm.95 4.725Z"/>
+              </svg>
+              :
+              <svg className='w-[22px] h-[22px]' 
+              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path fill="currentColor" 
+                  d="M13.35 20.13c-.76.69-1.93.69-2.69-.01l-.11-.1C5.3 15.27 1.87 12.16 2 8.28c.06-1.7.93-3.33 2.34-4.29c2.64-1.8 5.9-.96 7.66 1.1c1.76-2.06 5.02-2.91 7.66-1.1c1.41.96 2.28 2.59 2.34 4.29c.14 3.88-3.3 6.99-8.55 11.76l-.1.09z"></path>
+              </svg>
+              )
+            }
+          </button>
+        }
 
         <div className=' absolute w-max h-max pl-1 pr-1.5 bg-[#585858c2] backdrop-blur-sm
           rounded-lg text-[16px] flex items-center bottom-1 left-1.5'>
